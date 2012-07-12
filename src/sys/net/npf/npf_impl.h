1bf5e1dc55a2c2c260bf7aed7353eefa56b26907
mpp <pawlowski.mp@gmail.com>
2012-07-12 12:22:15 +0200

    I wont write what this fix is because it's shameful.

--- src/sys/net/npf/npf_impl.h
+++ src/sys/net/npf/npf_impl.h
@@ -191,8 +191,8 @@ void		npf_addr_mask(const npf_addr_t *, const npf_netmask_t,
 /* NPT adjustment calculation functions */
 uint16_t	npf_npt_adj_calc(const int, const npf_addr_t *, const npf_addr_t *);
 void		npf_npt_adj_add(int, npf_addr_t *, uint16_t);
-void		npf_npt_adj_sub(int, npf_addr_t *, uint16_t)
-bool		npf_addr_px_eq_chk(int px, npf_addr_t *, npf_addr_t *)
+void		npf_npt_adj_sub(int, npf_addr_t *, uint16_t);
+bool		npf_addr_px_eq_chk(int px, npf_addr_t *, npf_addr_t *);
 
 int		npf_tcpsaw(const npf_cache_t *, tcp_seq *, tcp_seq *,
 		    uint32_t *);
