f8b830088e5380d01acae84507e1c7a765fe8b04
mpp <pawlowski.mp@gmail.com>
2012-08-07 19:27:00 +0000

    sys/net/npf/npf_inet.c: in npf_v6_to_v4() I was refering to bad part of IPv6 address, spotted by spz. Moving npf_af_translator() here as sugested by rmind.

--- src/sys/net/npf/npf_inet.c
+++ src/sys/net/npf/npf_inet.c
@@ -262,10 +262,90 @@ void
 npf_v6_to_v4(npf_addr_t *a, npf_addr_t *b)
 {
 	/* I'm assuming that IPv4 is sitting in first 32bits of npf_addr_t */
-	a->s6_addr32[0] = b->s6_addr32[0];
+	a->s6_addr32[0] = b->s6_addr32[3];
 	a->s6_addr32[1] = 0x0; 
 	a->s6_addr32[2] = 0x0; 
 	a->s6_addr32[3] = 0x0; 
+}
+
+
+int
+npf_af_translator(npf_cache_t *npc, nbuf_t **nbuf,
+    npf_addr_t *src, npf_addr_t *dst)
+{
+	struct mbuf *m = *nbuf;
+	struct ip *ip4;
+	struct ip6_hdr *ip6;
+	size_t hlen;
+
+	/* Cut off the old header. Currently don't know what the
+	 * second parameter should be. Should I use offsetof()
+	 * somehow?
+	 */
+	m_adj(m, 0);
+
+	if (npc->npc_info == NPC_IP6) {
+		hlen = sizeof(*ip4);
+	} else {
+		hlen = sizeof(*ip6);
+	}
+
+	if ((m_prepend(m, hlen, M_DONTWAIT)) == NULL) {
+		nbuf = NULL;
+		return -1;
+	}
+
+	switch(npc->npc_info) {
+
+	case NPC_IP6: {
+		/* Translating from IPv6 to IPv4 */
+		ip4 = mtod(m, struct ip *);
+		bzero(ip4, hlen);
+		ip4->ip_v	= IPVERSION;
+		ip4->ip_hl	= hlen >> 2;
+		ip4->ip_len	= htons(hlen + npc->npc_ip.v6.ip6_plen);
+		/* ip4->ip_id	= random() & UINT16_MAX; ??? */
+		/* ip4->ip_off	= ???; */
+		/* Taking TTL from v6 hop limit */
+		ip4->ip_ttl	= npc->npc_ip.v6.ip6_hlim;
+		ip4->ip_p	= npc->npc_next_proto;
+		ip4->ip_src.s_addr	= (in_addr_t)src->s6_addr32[0];
+		ip4->ip_dst.s_addr	= (in_addr_t)dst->s6_addr32[0];
+		/* Now we have IPv4 header ready for action */
+		break;
+	}
+
+	case NPC_IP4: {
+		/* Translating from IPv4 to IPv6 */
+		ip6 = mtod(m, struct ip6_hdr *);
+		bzero(ip6, hlen);
+		ip6->ip6_vfc	= IPV6_VERSION;
+		/* ip6->ip6_flow	= ???; */
+		/* The size of IPv4 packet plus the difference between v6 
+		 * IPv6 and IPv4 header size.
+		 * I've made an assumption that the IPv4 options are unused.
+		 *
+		 * Important: The packet size is now 20 octets longer that is
+		 * it propably should be fragmented.
+		 */
+		ip6->ip6_plen	= npc->npc_ip.v4.ip_len + 20;
+		ip6->ip6_nxt	= npc->npc_next_proto;
+		/* Taking hop limit from v4 TTL if it's smaller than IPV6_DEFHLIM (?)*/
+		if (npc->npc_ip.v4.ip_ttl < IPV6_DEFHLIM)
+			ip6->ip6_hlim	= npc->npc_ip.v4.ip_ttl;
+		else
+			ip6->ip6_hlim	= IPV6_DEFHLIM;
+		memcpy(ip6->ip6_src.s6_addr, src->s6_addr, sizeof(struct in6_addr));
+		memcpy(ip6->ip6_dst.s6_addr, dst->s6_addr, sizeof(struct in6_addr));
+		/* Now we have IPv6 header ready for action */
+		break;
+	}
+
+	default:
+		return -1;
+	}
+
+	return 0;
 }
 
 /*
